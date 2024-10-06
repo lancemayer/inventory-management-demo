d80fd51585a0c4c84ebf8475a26a5e8d937a12af
Lance Klinger <lance.d.klinger@gmail.com>
2024-10-06 17:51:56 -0400

    add supabase setup to backend

---
+++ backend/Item.cs
@@ -0,0 +1,30 @@
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+[Table("item")]
+class Item : BaseModel
+{
+	[PrimaryKey("id", false)]
+	public string Id { get; set; }
+
+	[Column("created_at")]
+	public DateTime CreatedAt { get; set; }
+
+	[Column("deleted_at")]
+	public DateTime? DeletedAt { get; set; }
+
+	[Column("name")]
+	public string Name { get; set; }
+
+	[Column("description")]
+	public string? Description { get; set; }
+}
+
+class ItemModel
+{
+	public string Id { get; set; }
+	public DateTime CreatedAt { get; set; }
+	public DateTime? DeletedAt { get; set; }
+	public string Name { get; set; }
+	public string? Description { get; set; }
+}
---
+++ backend/Program.cs
@@ -0,0 +1,81 @@
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using dotenv.net;
+
+
+DotEnv.Load();
+
+var url = Environment.GetEnvironmentVariable("SUPABASE_URL");
+var key = Environment.GetEnvironmentVariable("SUPABASE_KEY");
+
+var options = new Supabase.SupabaseOptions
+{
+    AutoConnectRealtime = true
+};
+
+var supabase = new Supabase.Client(url, key, options);
+await supabase.InitializeAsync();
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.MapGet("/items", async () =>
+{
+    var result = await supabase.From<Item>().Get();
+    var item = result.Models;
+
+    List<ItemModel> itemModels = item.Select(w => new ItemModel
+    {
+        Id = w.Id,
+        CreatedAt = w.CreatedAt,
+        DeletedAt = w.DeletedAt,
+        Name = w.Name,
+        Description = w.Description
+    }).ToList();
+
+    return itemModels;
+})
+.WithName("GetItems")
+.WithOpenApi();
+
+app.MapPost("/create-item", async (NewItem item) =>
+{
+    var model = new Item
+    {
+        CreatedAt = DateTime.Now,
+        DeletedAt = null,
+        Name = item.Name,
+        Description = item.Description
+    };
+
+    await supabase.From<Item>().Insert(model);
+
+    return $"Inserted {item.Name}";
+})
+.WithName("CreateItem")
+.WithOpenApi();
+
+app.Run();
+
+public record NewItem(string Name, string? Description);
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary, string? Test)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
